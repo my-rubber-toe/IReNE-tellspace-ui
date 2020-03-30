64da1899803aca10c6643bb144d8e50933365e94
alberto-canela <alberto.canela@upr.edu>
2020-03-30 12:11:12 -0400

    feat((editors)): added CKEditor5 Autosave plugin and configured ng model

--- src/app/modules/document-edition/pages/section-editor/section-editor.component.ts
+++ src/app/modules/document-edition/pages/section-editor/section-editor.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
-import * as ClassicEditor from "@ckeditor/ckeditor5-build-classic";
-import { ContentSection } from "@app/models/content-section";
+import {
+  Component,
+  OnInit,
+} from "@angular/core";
+
 import { DocumentEditionService } from "@app/core/services/document-edition.service";
-import { CaseDocument } from "@app/models/case-document";
-import { Observable } from "rxjs";
-import { switchMap } from "rxjs/operators";
 import { ActivatedRoute } from "@angular/router";
 import { FormControl } from "@angular/forms";
+import * as ClassicEditorWithAutosave from "@app/../assets/ckeditor.js";
 
 @Component({
   selector: "app-section-editor",
@@ -14,22 +14,40 @@ import { FormControl } from "@angular/forms";
   styleUrls: ["./section-editor.component.scss"]
 })
 export class SectionEditorComponent implements OnInit {
-  public Editor = ClassicEditor;
+  public Editor = ClassicEditorWithAutosave;
+
+  public EditorConfig = {
+    autosave: {
+      // The minimum amount of time the Autosave plugin is waiting after the last data change.
+      waitingTime: 5000,
+      save: editor => this.saveData(editor.getData())
+    }
+  };
+
+  sectionInitialData: string;
+  activeSection: number;
+
+  public model = {
+    editorData: "<p>Hello, world!</p>"
+  };
+  title = new FormControl("");
   constructor(
     private editService: DocumentEditionService,
     private route: ActivatedRoute
   ) {}
 
-  activeDoc$: Observable<CaseDocument>;
-  activeDoc: CaseDocument;
-  activeSection: number;
-
-  title = new FormControl("");
-
   ngOnInit(): void {
-    this.route.paramMap.subscribe(
-      params => (this.activeSection = +params.get("secid"))
-    );
-    this.editService.getDocumentStream().subscribe(x => (this.activeDoc = x));
+    this.route.paramMap.subscribe(params => {
+      this.activeSection = +params.get("secid");
+      this.model.editorData = `<p>This is section ${this.activeSection}</p>`;
+      console.log(
+        "section_editor refreshed with section number: ",
+        this.activeSection
+      );
+    });
+  }
+
+  saveData(data: string) {
+    console.log("saved", data);
   }
 }

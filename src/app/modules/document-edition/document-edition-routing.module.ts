d59f9d7ec6787a32fc7dcfd6e9cd30795409830b
alberto-canela <alberto.canela@upr.edu>
2020-04-14 23:11:30 -0400

    fix(editor): added doc edition route to lazy loaded routes, missing from last commit
    Last commit was missing a reference to the doc sideboard, this commit adds the document edition
    component to the routing module of the doc edition module.

--- src/app/modules/document-edition/document-edition-routing.module.ts
+++ src/app/modules/document-edition/document-edition-routing.module.ts
@@ -2,14 +2,21 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { DocumentOptionsComponent } from "./pages/document-options/document-options.component";
 import { SectionEditorComponent } from "./pages/section-editor/section-editor.component";
+import { DocumentEditionComponent } from "./document-edition.component";
 
 const routes: Routes = [
-  { path: "", pathMatch: "full", component: DocumentOptionsComponent },
-  { path: "s/:secid", component: SectionEditorComponent }
+  {
+    path: "",
+    component: DocumentEditionComponent,
+    children: [
+      { path: "", pathMatch: "full", component: DocumentOptionsComponent },
+      { path: "s/:secid", component: SectionEditorComponent },
+    ],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class DocumentEditionRoutingModule {}

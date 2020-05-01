7191c37e96546f5bb869f90a18e22403a417020f
alberto-canela <alberto.canela@upr.edu>
2020-05-01 15:39:15 -0400

    style(editors): added contrast to buttons and improved layout. Editor view look different than view
    Buttons looked without contrast and confused users if clickable or not. Changed all buttons to
    mat-raised buttons. Fixed adaptavility of editors, by changing scss. Eliminated edit cues from non
    editing modes on option editors.

--- src/app/modules/document-edition/components/date-timeline-editor/date-timeline-editor.component.ts
+++ src/app/modules/document-edition/components/date-timeline-editor/date-timeline-editor.component.ts
@@ -27,18 +27,13 @@ export class DateTimelineEditorComponent implements OnInit {
 
   editingIncidentDate: boolean = false;
 
-  minDateStart: Date;
-  //maxDateStart: Date; //Seting max date of start to be the current end date
-  // minDateEnd: Date; //Setting min date for end to the current start date
   maxDateEnd: Date;
 
   constructor(
     private fb: FormBuilder,
     private edition: DocumentEditionService
   ) {
-    // Set tminimum and maximun dates
-    const currentYear = new Date().getFullYear();
-    this.minDateStart = new Date(currentYear - 20, 0, 1);
+    // Set minimum and maximun dates
     this.maxDateEnd = new Date();
   }
 
